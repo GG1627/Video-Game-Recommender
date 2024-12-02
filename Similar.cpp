a72ab0c3454e6ff53782e347a591484e5bb258f6
GG1627 <gael.garcia@ufl.edu>
2024-12-02 00:59:25 -0500

    Fixed Extract Platform

--- Similar.cpp
+++ Similar.cpp
@@ -2,15 +2,13 @@
 
 vector<Game> Similar::extractGenre(const string &gameName) {
     vector<Game> filteredGames;
-    unordered_set<string> targetGenres;
+    unordered_set<string> targetGenres = getGenres(gameName);
 
-    targetGenres = getGenres(gameName);
-
-    for(int i = 0; i < gamesRef.size(); i++) {
+    for (int i = 0; i < gamesRef.size(); i++) {
         unordered_set<string> currentGameGenres = getGenres(gamesRef[i].name);
 
-        for(auto it = currentGameGenres.begin(); it != currentGameGenres.end(); ++it) {
-            if(targetGenres.find(*it) != targetGenres.end()) {
+        for (auto it = currentGameGenres.begin(); it != currentGameGenres.end(); ++it) {
+            if (targetGenres.find(*it) != targetGenres.end()) {
                 filteredGames.push_back(gamesRef[i]);
                 break;
             }
@@ -20,17 +18,17 @@ vector<Game> Similar::extractGenre(const string &gameName) {
     return filteredGames;
 }
 
-vector<Game> Similar::extractPlatform(const string &gameName, vector<Game> filterdGames) {
-    vector<Game> finalFilteredGames;
 
+vector<Game> Similar::extractPlatform(const string &gameName, vector<Game> filteredGames) {
+    vector<Game> finalFilteredGames;
     unordered_set<string> targetPlatforms = getPlatforms(gameName);
 
-    for(int i = 0; i < filterdGames.size(); i++) {
-        unordered_set<string> currentGamePlatforms = getPlatforms(gamesRef[i].name);
+    for (int i = 0; i < filteredGames.size(); i++) {
+        unordered_set<string> currentGamePlatforms = getPlatforms(filteredGames[i].name);
 
-        for(auto it = currentGamePlatforms.begin(); it != currentGamePlatforms.end(); ++it) {
-            if(targetPlatforms.find(*it) != targetPlatforms.end()) {
-                finalFilteredGames.push_back(gamesRef[i]);
+        for (auto it = currentGamePlatforms.begin(); it != currentGamePlatforms.end(); ++it) {
+            if (targetPlatforms.find(*it) != targetPlatforms.end()) {
+                finalFilteredGames.push_back(filteredGames[i]);
                 break;
             }
         }
@@ -38,4 +36,5 @@ vector<Game> Similar::extractPlatform(const string &gameName, vector<Game> filte
 
     return finalFilteredGames;
 }
+
 
